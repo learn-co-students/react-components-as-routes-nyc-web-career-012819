91fcf4ed1e5ce57ce7bdb3e7e97b2ff5d86b4ebd
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-04-11 07:39:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+import { BrowserRouter as Router, Route, NavLink } from  'react-router-dom';
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () => {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={link} activeStyle={{
+          background: 'darkblue'}}
+          >Home</NavLink>
+        <NavLink
+          to="/about"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+          >About</NavLink>
+        <NavLink
+          to="/login"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+          >Login</NavLink>
+      </div>)
+}
+
 const Home = () => {
   return (
     <div>
@@ -9,7 +49,41 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+const About = () => {
+  return (
+    <div>
+      <h1>This is the about component!</h1>
+    </div>
+  )
+}
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  )
+}
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      {/*<Route exact path="/" component={Home}/>*/}
+      <Navbar />
+      <Route exact path="/" render={Home}/>
+      <Route exact path="/about" render={About}/>
+      <Route exact path="/login" render={Login}/>
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
